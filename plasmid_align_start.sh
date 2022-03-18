30911c17d48c081592e1de4429bef57bf305d849
benjaminmartin02 <57191064+benjaminmartin02@users.noreply.github.com>
2022-03-18 08:54:23 -0400

    Update plasmid_align_start.sh

--- plasmid_align_start.sh
+++ plasmid_align_start.sh
@@ -3,17 +3,27 @@
 SEQ=$1
 START=$2
 
+echo "Starting plasmid file is: $SEQ"
+echo "Start sequence input: $START"
+
 #convert to uppercase (if it's not already)
 val=$(echo "$START" | tr '[:lower:]' '[:upper:]')
 
+echo "Start sequence after conversion to uppercase (if needed): $val"
+
+#make temp file with header
 head -1 $SEQ > ${SEQ}_fasta_header.txt
 
-#remove first line and match to sequence, also include code snippet to convert plasmid sequence to uppercase
+#make temp file with sequence, also include code snippet to convert plasmid sequence to uppercase
+sed '1d' $SEQ | tr '[:lower:]' '[:upper:]' > ${SEQ}_sequence
 
-MATCHED_ROW_NUM=`sed '1d' $SEQ | tr '[:lower:]' '[:upper:]' | perl -pe "s/$val/\n$val$1/" | wc -l`
+
+#remove first line and match to sequence
+
+MATCHED_ROW_NUM=`perl -pe "s/$val/\n$val$1/" ${SEQ}_sequence | wc -l`
 
 #check how many times the start sequence was matched:
-ORIG_ROW_NUM=`sed '1d' $SEQ | wc -l`
+ORIG_ROW_NUM=`cat ${SEQ}_sequence | wc -l`
 TIMES_MATCHED=`expr $MATCHED_ROW_NUM - $ORIG_ROW_NUM`
 
 echo "Matched start sequence in plasmid ${TIMES_MATCHED} time"
@@ -21,8 +31,8 @@ echo "Matched start sequence in plasmid ${TIMES_MATCHED} time"
 if [ $TIMES_MATCHED=1 ]; then  
 
    #make variables for sequence before or after specified start sequence
-   SECOND=`sed '1d' $SEQ | tr '[:lower:]' '[:upper:]' | perl -pe "s/$val/\n$val$1/" | head -1`
-   FIRST=`sed '1d' $SEQ | tr '[:lower:]' '[:upper:]' | perl -pe "s/$val/\n$val$1/" | head -2 | tail -1`
+   SECOND=`perl -pe "s/$val/\n$val/" ${SEQ}_sequence | head -1`
+   FIRST=`perl -pe "s/$val/\n$val/" ${SEQ}_sequence | head -2 | tail -1`
    
    #paste sequences together and cat back in the fastq header
    echo -e "${FIRST}${SECOND}" | cat ${SEQ}_fasta_header.txt - > Realigned_${SEQ}
@@ -32,3 +42,4 @@ else
 fi
 
 rm ${SEQ}_fasta_header.txt
+rm ${SEQ}_sequence
